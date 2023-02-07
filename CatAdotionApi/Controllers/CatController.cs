e5479e297d8f73add62861eef25c560a9ac9a3fd
Ariel Vieira <arielvieira65@gmail.com>
2023-02-07 11:50:46 -0300

    Feat: Add update cat for id

--- CatAdotionApi/Controllers/CatController.cs
+++ CatAdotionApi/Controllers/CatController.cs
@@ -9,7 +9,7 @@ namespace CatAdotionApi.Controllers;
 public class CatController : ControllerBase
 {
 	private CatAdoptionContext _context;
-	public CatController(CatAdoptionContext context)
+    public CatController(CatAdoptionContext context)
 	{
 		_context = context;
 	}
@@ -20,14 +20,14 @@ public class CatController : ControllerBase
 		return _context.Cats;
 	}
 
-    [HttpPost]
-    [ProducesResponseType(StatusCodes.Status201Created)]
-    public IActionResult Create([FromBody] Cat cat)
+	[HttpPost]
+	[ProducesResponseType(StatusCodes.Status201Created)]
+	public IActionResult Create([FromBody] Cat cat)
 	{
 		_context.Cats.Add(cat);
 		_context.SaveChanges();
-        return CreatedAtAction(nameof(Show), new { id = cat.Id }, cat); // Vai informar pro usuário em qual caminho ele pode encontrar o resurso criado
-    }
+		return CreatedAtAction(nameof(Show), new { id = cat.Id }, cat); // Vai informar pro usuário em qual caminho ele pode encontrar o resurso criado
+	}
 
 	[HttpGet("{id}")]
 	public IActionResult Show(int id)
@@ -36,4 +36,23 @@ public class CatController : ControllerBase
 		if (cat == null) return NotFound();
 		return Ok(cat);
 	}
+
+	// TODO
+	// Implementar DTOs e utilizar AutoMapper para melhorar esse procedimento
+	[HttpPut("{id}")]
+    public IActionResult Update(int id, [FromBody] Cat updatedCat)
+    {
+        var cat = _context.Cats.FirstOrDefault(cat => cat.Id == id);
+		if (cat == null) return NotFound();
+		
+		cat.Name= updatedCat.Name;
+		cat.Age = updatedCat.Age;
+		cat.Color = updatedCat.Color;
+		cat.Weight = updatedCat.Weight;
+		cat.Breed = updatedCat.Breed;
+		cat.Gender = updatedCat.Gender;
+
+		_context.SaveChanges();
+		return NoContent();
+    }
 }
