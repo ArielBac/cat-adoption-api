422e57c4614f39e8da12ac85fe3af536e325e6d3
Ariel Vieira <arielvieira65@gmail.com>
2023-02-07 10:52:28 -0300

    Feat: Add show cat for id and create cat

--- CatAdotionApi/Controllers/CatController.cs
+++ CatAdotionApi/Controllers/CatController.cs
@@ -6,7 +6,7 @@ namespace CatAdotionApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class CatController
+public class CatController : ControllerBase
 {
 	private CatAdoptionContext _context;
 	public CatController(CatAdoptionContext context)
@@ -15,8 +15,25 @@ public class CatController
 	}
 
 	[HttpGet]
-	public IEnumerable<Cat> index()
+	public IEnumerable<Cat> Index()
 	{
 		return _context.Cats;
+	}
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public IActionResult Create([FromBody] Cat cat)
+	{
+		_context.Cats.Add(cat);
+		_context.SaveChanges();
+        return CreatedAtAction(nameof(Show), new { id = cat.Id }, cat); // Vai informar pro usuário em qual caminho ele pode encontrar o resurso criado
+    }
+
+	[HttpGet("{id}")]
+	public IActionResult Show(int id)
+	{
+		var cat = _context.Cats.FirstOrDefault(cat => cat.Id == id);
+		if (cat == null) return NotFound();
+		return Ok(cat);
 	}
 }
