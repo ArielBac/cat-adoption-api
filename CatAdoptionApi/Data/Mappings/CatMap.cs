a3b6daf3c6996fd3a4d6dc9648c7f99942d720c1
Ariel Vieira <arielvieira65@gmail.com>
2023-02-13 14:12:20 -0300

    Refactor: Add models mapping

---
+++ CatAdoptionApi/Data/Mappings/CatMap.cs
@@ -0,0 +1,49 @@
+﻿using CatAdoptionApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CatAdoptionApi.Data.Mappings
+{
+    public class CatMap : IEntityTypeConfiguration<Cat>
+    {
+        public void Configure(EntityTypeBuilder<Cat> builder)
+        {
+            builder.ToTable("cats");
+
+            builder.Property(p => p.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnType("int");
+
+            builder.HasKey("Id");
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(30)
+                .HasColumnType("varchar(30)");
+
+            builder.Property(p => p.Age)
+                .HasColumnType("int");
+
+            builder.Property(p => p.Breed)
+                .HasMaxLength(30)
+                .HasColumnType("varchar(30)");
+
+            builder.Property(p => p.Color)
+                .IsRequired()
+                .HasMaxLength(30)
+                .HasColumnType("varchar(30)");
+
+            builder.Property(p => p.Gender)
+                .IsRequired()
+                .HasMaxLength(1)
+                .HasColumnType("char(1)")
+                .IsFixedLength();
+
+            builder.Property(p => p.Weight)
+                .HasColumnType("double");
+
+            builder.HasMany(p => p.Vaccines)
+                .WithOne();
+        }
+    }
+}
---
+++ CatAdoptionApi/Data/Mappings/VaccineMap.cs
@@ -0,0 +1,43 @@
+﻿using CatAdoptionApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CatAdoptionApi.Data.Mappings
+{
+    public class VaccineMap : IEntityTypeConfiguration<Vaccine>
+    {
+        public void Configure(EntityTypeBuilder<Vaccine> builder)
+        {
+            builder.ToTable("vaccines");
+
+            builder.Property(p => p.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.CatId)
+                .IsRequired();
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasColumnType("varchar(30)")
+                .HasMaxLength(30);
+
+            builder.Property(p => p.Producer)
+                .IsRequired()
+                .HasColumnType("varchar(50)")
+                .HasMaxLength(50);
+
+            builder.Property(p => p.Applicated_at)
+                .IsRequired()
+                .HasColumnType("datetime");
+
+            // Da forma como estão minhas entidades, o enity constrói esse relacionamento por padrão
+            // Explicitado para fins de entendimento e aprendizado
+            builder.HasOne(p => p.Cat)
+                .WithMany()
+                .HasForeignKey(p => p.CatId);
+        }
+    }
+}
