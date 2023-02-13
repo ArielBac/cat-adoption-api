71bd9431a55ec828c7ea5237cd75b7d6127b1eb8
Ariel Vieira <arielvieira65@gmail.com>
2023-02-13 09:41:28 -0300

    Feat: Add vaccine model

---
+++ CatAdoptionApi/Models/Vaccine.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatAdoptionApi.Models
+{
+    public class Vaccine
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        [StringLength(30, ErrorMessage = "O campo Nome não deve exceder 30 caracteres")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "O campo Fabricante é obrigatório")]
+        [StringLength(50, ErrorMessage = "O campo Fabricante não deve exceder 50 caracteres")]
+        public string Producer { get; set; } = null!;
+    }
+}
