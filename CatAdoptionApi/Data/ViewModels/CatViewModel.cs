8954f52367f1ec716b26a887b7e53de50f7d2908
Ariel Vieira <arielvieira65@gmail.com>
2023-02-14 14:28:28 -0300

    Fix: fix commits messages

---
+++ CatAdoptionApi/Data/ViewModels/CatViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CatAdoptionApi.Data.ViewModels;
+
+public class CatViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Breed { get; set; }
+    public double Weight { get; set; }
+    public string Color { get; set; } = null!;
+    public int Age { get; set; }
+    public string Gender { get; set; } = null!;
+}
---
+++ CatAdoptionApi/Data/ViewModels/VaccineViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CatAdoptionApi.Data.ViewModels;
+
+public class VaccineViewModel
+{
+    public int Id { get; set; }
+
+    public int CatId { get; set; }
+
+    public DateTime Applicated_at { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Producer { get; set; } = null!;
+}
