630ee3e254e237366c526022375d81535f6f6cc4
Ariel Vieira <arielvieira65@gmail.com>
2023-02-14 14:32:34 -0300

    Feat: Add profiles

---
+++ CatAdoptionApi/Profiles/VaccineProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CatAdoptionApi.Data.Dtos.Vaccines;
+using CatAdoptionApi.Data.ViewModels;
+using CatAdoptionApi.Models;
+
+namespace CatAdoptionApi.Profiles
+{
+    public class VaccineProfile : Profile
+    {
+        public VaccineProfile() 
+        {
+            CreateMap<Vaccine, ReadVaccineDto>();
+            CreateMap<ReadVaccineDto, Vaccine>();
+            CreateMap<Vaccine, VaccineViewModel>();
+        }
+
+    }
+}
