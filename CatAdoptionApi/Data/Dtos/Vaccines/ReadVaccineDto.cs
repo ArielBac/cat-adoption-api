1f1541eb181fa44be954792e94caa3fab7ae5dff
Ariel Vieira <arielvieira65@gmail.com>
2023-02-14 09:11:06 -0300

    Feat: Add vaccine dtos

---
+++ CatAdoptionApi/Data/Dtos/Vaccines/ReadVaccineDto.cs
@@ -0,0 +1,20 @@
+﻿using CatAdoptionApi.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CatAdoptionApi.Data.Dtos.Vaccines;
+
+public class ReadVaccineDto
+{
+    public int Id { get; set; }
+
+    public int CatId { get; set; }
+
+    public DateTime Applicated_at { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Producer { get; set; } = null!;
+
+    public Cat Cat { get; set; }
+}
+
