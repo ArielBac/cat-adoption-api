fa5e071179929fa16c4af3b27eb139ce352df167
Ariel Vieira <arielvieira65@gmail.com>
2023-02-23 09:31:41 -0300

    Docs: Alter MySQL files

---
+++ MySqlScripts/01.002.CreateCatTable.sql
@@ -0,0 +1,13 @@
+#MySQL
+
+USE cat_adoption_db;
+
+CREATE TABLE cats(
+	Id INT PRIMARY KEY AUTO_INCREMENT,
+	Name VARCHAR(30) NOT NULL, 
+	Breed VARCHAR(30),
+	Weight DOUBLE NOT NULL,
+	Color VARCHAR(30) NOT NULL,
+	Age INT NOT NULL,
+	Gender CHAR NOT NULL
+);
