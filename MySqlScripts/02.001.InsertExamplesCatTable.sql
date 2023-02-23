856c7e459dff8f3e0d0f4545fba6d3d7481e1a29
Ariel Vieira <arielvieira65@gmail.com>
2023-02-23 15:22:54 -0300

    Docs: Update MySQL scripts

--- MySqlScripts/02.001.InsertExamplesCatTable.sql
+++ MySqlScripts/02.001.InsertExamplesCatTable.sql
@@ -2,11 +2,31 @@
 
 USE cat_adoption_db;
 
-INSERT INTO cats (Name, Breed, Weight, Color, Age, Gender)
-VALUES 
-	("Pingo", "Viralata", 2.5, "Preto", 2, 'M'),
-  	("Josefina", "Viralata", 2, "Branco", 1, 'F'),
-  	("Carlito", "Viralata", 3, "Marrom", 5, 'M'),
-  	("Filomena", "Viralata", 2.6, "Mesclado (branco e preto)", 4, 'F'),
-  	("Polenta", "Viralata", 2.1, "Marrom", 1, 'F'),
-  	("Fernando", "Viralata", 2.9, "Branco", 2, 'M');
+DROP PROCEDURE IF EXISTS InsertCatIfNotExistsProcedure;
+
+DELIMITER $$
+CREATE PROCEDURE InsertCatIfNotExistsProcedure (IN NameParam VARCHAR(30), BreedParam VARCHAR(30), WeightParam DOUBLE, ColorParam VARCHAR(30), AgeParam INT, GenderParam CHAR(1))
+BEGIN
+	SET @hasCat = (SELECT count(*) FROM cats WHERE (
+		Name = NameParam AND 
+        Breed = BreedParam AND 
+        Weight = WeightParam AND
+        Color = ColorParam AND
+        Age = AgeParam AND
+        Gender = GenderParam));
+    
+    IF (@hasCat != 0) THEN
+		SET @message = concat("O gatinho ", NameParam, ", com esses atributos, já existe");
+		SELECT @message;
+	ELSE
+		INSERT INTO cats (Name, Breed, Weight, Color, Age, Gender)
+		VALUES(NameParam, BreedParam, WeightParam, ColorParam, AgeParam, GenderParam);
+    END IF;
+END $$
+DELIMITER ;
+
+CALL InsertCatIfNotExistsProcedure("Pingo", "Viralata", 2.5, "Preto", 2, 'M');
+CALL InsertCatIfNotExistsProcedure("Josefina", "Viralata", 2, "Branco", 1, 'F');
+CALL InsertCatIfNotExistsProcedure("Carlito", "Viralata", 3, "Marrom", 5, 'M');
+CALL InsertCatIfNotExistsProcedure("Filomena", "Viralata", 2.6, "Mesclado (branco e preto)", 4, 'F');
+CALL InsertCatIfNotExistsProcedure("Polenta", "Viralata", 2.1, "Marrom", 1, 'F');
